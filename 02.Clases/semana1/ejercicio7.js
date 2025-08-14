82a525e41db213a184b1a2dcaf66df5b8b9496f3
Rodrigo Leaños Bermejo <rodrigoBermejo@users.noreply.github.com>
2025-08-14 12:17:08 -0600

    Update ejercicio7.js

--- 02.Clases/semana1/ejercicio7.js
+++ 02.Clases/semana1/ejercicio7.js
@@ -4,6 +4,16 @@ const { ask } = require('../helpers/input');
 async function main() {
   const number = Number(await ask("Ingresa un número:"));
 
+  if (number % 3 !== 0 && number % 5 !== 0) {
+    console.log('No es múltiplo de 3 o 5');
+  } else if (number % 3 === 0 && number % 5 !== 0) {
+    console.log('Es múltiplo de 3');
+  } else if (number % 5 === 0 && number % 3 !== 0) {
+    console.log('Es múltiplo de 5');
+  } else {
+    console.log('Es múltiplo de 3 y 5');
+  }
+
 }
 
 main();
