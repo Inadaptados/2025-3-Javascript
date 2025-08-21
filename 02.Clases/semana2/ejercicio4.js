e45fea617ac61335610f57cb0c4fd7c367113988
Rodrigo Leaños Bermejo <rodrigoBermejo@users.noreply.github.com>
2025-08-20 21:02:17 -0600

    update nombres con vocal

--- 02.Clases/semana2/ejercicio4.js
+++ 02.Clases/semana2/ejercicio4.js
@@ -37,11 +37,29 @@ function resumenEstadistico(numeros) {
   return { promedio, mayor, menor };
 }
 
+function nombresConVocal(nombres) {
+  const vocales = ['a', 'e', 'i', 'o', 'u', 'á', 'é', 'í', 'ó', 'ú'];
+  const resultado = [];
+
+  for (let i = 0; i < nombres.length; i++) {
+    let primerLetra = nombres[i][0].toLowerCase();
+    // for (let j = 0; j < vocales.length; j++) {
+    //   if (vocales[j] === primerLetra) {
+    //     resultado.push(nombres[i]);
+    //   }
+    // }
+    if (vocales.includes(primerLetra)) {
+      resultado.push(nombres[i]);
+    }
+  }
+  return resultado;
+}
+
 async function main() {
   const edades = [20, 18, 25, 30, 22];
   const lista = [5, 20, 8, 99, 3];
   const datos = [70, 80, 90, 100, 85];
-  const nombres = ['Ana', 'Esteban', 'Luis', 'Oscar', 'María'];
+  const nombres = ['Ana', 'Esteban', 'Luis', 'Óscar', 'María'];
 
   console.log(`Promedio de edades = ${obtenerPromedio(edades)}`);
   console.log(`El número mayor de: ${lista} = ${obtenerMayor(lista)}`);
@@ -52,6 +70,7 @@ async function main() {
   console.log(`Promedio: ${estadisticas.promedio}`);
   console.log(`Mayor: ${estadisticas.mayor}`);
   console.log(`Menor: ${estadisticas.menor}`);
+  console.log(`Nombres con vocal: ${nombresConVocal(nombres)}`);
 }
 
 main();
