52b79f88d57f17bb4572359746d31068a5f84d6d
Rodrigo Leaños Bermejo <rodrigoBermejo@users.noreply.github.com>
2025-08-25 19:56:34 -0600

    resolución reto 8

--- 01.Retos/reto-08-calificaciones/index.js
+++ 01.Retos/reto-08-calificaciones/index.js
@@ -1,7 +1,45 @@
 const { ask } = require('./helpers/input');
 
-async function main() {
+function obtenerPromedio(numeros) {
+  let total = 0;
+  for (let i = 0; i < numeros.length; i++) {
+    total = total + numeros[i];
+  }
+  return (total / numeros.length);
+}
 
+function obtenerAprobados(numeros) {
+  let aprobados = 0;
+  let reprobados = 0;
+  for (let i = 0; i < numeros.length; i++) {
+    if (numeros[i] >= 70) {
+      aprobados++;
+    } else {
+      reprobados++;
+    }
+  }
+  return { aprobados, reprobados };
+}
+
+function analizarCalificaciones(numeros) {
+  const { aprobados, reprobados } = obtenerAprobados(numeros);
+  const promedio = Math.floor(obtenerPromedio(numeros));
+  const califAlta = Math.max(...numeros);
+  const califBaja = Math.min(...numeros);
+
+  return {
+    aprobados,
+    reprobados,
+    promedio,
+    califAlta,
+    califBaja
+  };
+}
+
+async function main() {
+  const calificaciones = [90, 89, 34, 45, 67, 89, 23, 12, 45, 89, 8, 90];
+  const resultado = analizarCalificaciones(calificaciones);
+  console.log(resultado);
 }
 
 main();
